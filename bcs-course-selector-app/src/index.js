b715c3446747b0b6cb960856c9503a3244fba922
Ben Cheung <chessben88@gmail.com>
2020-04-02 00:44:15 -0700

    Added modules, and test.js

--- bcs-course-selector-app/src/index.js
+++ bcs-course-selector-app/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './Test';
 import * as serviceWorker from './serviceWorker';
+
+
+import { Provider } from 'react-redux';
+import store from './redux/store/index';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +15,15 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  <Provider store={store}>
+  <Test />
+  </Provider>,
+  document.getElementById('root')
+
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ bcs-course-selector-app/src/routes/Routes.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { dashboard as dashboardRoutes, auth as authRoutes } from "./index";
+
+import DashboardLayout from "../layouts/Dashboard";
+import AuthLayout from "../layouts/Auth";
+import Page404 from "../pages/auth/Page404";
+
+const childRoutes = (Layout, routes) =>
+  routes.map(({ children, path, component: Component }, index) =>
+    children ? (
+      // Route item with children
+      children.map(({ path, component: Component }, index) => (
+        <Route
+          key={index}
+          path={path}
+          exact
+          render={props => (
+            <Layout>
+              <Component {...props} />
+            </Layout>
+          )}
+        />
+      ))
+    ) : (
+      // Route item without children
+      <Route
+        key={index}
+        path={path}
+        exact
+        render={props => (
+          <Layout>
+            <Component {...props} />
+          </Layout>
+        )}
+      />
+    )
+  );
+
+const Routes = () => (
+  <Router>
+    <Switch>
+      {childRoutes(DashboardLayout, dashboardRoutes)}
+      {childRoutes(AuthLayout, authRoutes)}
+      <Route
+        render={() => (
+          <AuthLayout>
+            <Page404 />
+          </AuthLayout>
+        )}
+      />
+    </Switch>
+  </Router>
+);
+
+export default Routes;
