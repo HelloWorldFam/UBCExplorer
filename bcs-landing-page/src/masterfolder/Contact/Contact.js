43c02f57f829ccab20bc172f5da406d7a4a0bea2
JamesPark <45812677+jpjp12@users.noreply.github.com>
2020-04-12 21:00:07 -0700

    first commit

---
+++ bcs-landing-page/src/masterfolder/Contact/Contact.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import 'materialize-css/dist/css/materialize.min.css'
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <section id="contact" className="section section-contact scrollspy">
+                <div className="container">
+                    <div className="row">
+                    <div className="col s12 m6">
+                        <div className="card-panel purple white-text center">
+                        <span class="material-icons">mail</span>
+                        <h4>Email</h4>
+                        <p>Please email us for any questions:_____ @gmail.com</p>
+                        </div>
+                    </div>
+                    <div className="col s12 m6">
+                        <div className="card-panel white center">
+                        <i class="material-icons">emoji_people</i>
+                            <h4>Our Vision</h4>
+                            {/* <form>
+                                <div className="input-field">
+                                    <input type="text" id="vision" className="validate" /> */}
+                                        {/* <label for="vision" className="purple-text">With the BCS course-selector App, we hope to deliver a one-in-all website</label> */}
+                                        <p>With the BCS course-selector App, we hope to deliver a one-in-all with course planning</p>
+                                        <p>More description to be added here</p>
+                                </div>
+
+
+                            {/* </form> */}
+
+                        </div>
+                    </div>
+                    </div>
+                {/* </div> */}
+
+
+            </section>
+
+        )
+    }
+}
---
+++ bcs-landing-page/src/masterfolder/Header/Header.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import 'materialize-css/dist/css/materialize.css';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+const SmoothScroll = () => {
+    return (
+        <section>
+            <div className="navbar-fixed">
+                <nav className="purple">
+                    <div className="container">
+                        <div className="nav-wrapper">
+                            <AnchorLink href="!#" className="brand-logo">BCS Course Selector App</AnchorLink>
+                            <AnchorLink href="!#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></AnchorLink>
+                                <ul className="right hide-on-med-and-down">
+                                    <li><AnchorLink href="/home">Home</AnchorLink></li>
+                                    <li><AnchorLink href="/about">About</AnchorLink></li>
+                                    <li><AnchorLink href="/contact">Contact</AnchorLink></li>
+                                    <li><AnchorLink href="/SignIn">SignIn</AnchorLink></li>
+                               </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <ul class="sidenav" id="mobile-demo">
+                <li><AnchorLink href="/home">Home</AnchorLink></li>
+                <li><AnchorLink href="/about">About</AnchorLink></li>
+                <li><AnchorLink href="/contact">Contact</AnchorLink></li>
+                <li><AnchorLink href="/banner">Banner</AnchorLink></li>
+
+            </ul>
+        </section>
+
+    )
+}
+
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <SmoothScroll />
+            </div>
+        )
+    }
+}
