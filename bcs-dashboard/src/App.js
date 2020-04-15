22942f7b3582c4f2498c0cd7a0e45fefdd774097
Scott <scott.m.king@outlook.com>
2020-04-14 20:37:31 -0700

    Setup backend for Material-UI and hooked up signup page

--- bcs-dashboard/src/App.js
+++ bcs-dashboard/src/App.js
@@ -1,22 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Home from "./components/home.component";
-import SignupScreen from "./components/signup-screen.component";
-import Dashboard from "./components/dashboard.component";
-
-import Button from '@material-ui/core/Button';
-import Drawer from '@material-ui/core/Drawer';
-
-import TimeTable from "./components/timetable.component.js";
-
-=======
 import logo from './logo.svg';
 import './App.css';
->>>>>>> 5474abeaf243d274b25c8695798ea9ac8b6ec76f
 
 function App() {
   return (
--- bcs-dashboard/src/pages/auth/SignUp.js
+++ bcs-dashboard/src/pages/auth/SignUp.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-
-import Helmet from 'react-helmet';
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
+import Helmet from "react-helmet";
 
 import {
   FormControl,
@@ -10,45 +11,80 @@ import {
   InputLabel,
   Button as MuiButton,
   Paper,
-  Typography
 } from "@material-ui/core";
 import { spacing } from "@material-ui/system";
 
 const Button = styled(MuiButton)(spacing);
 
 const Wrapper = styled(Paper)`
-  padding: ${props => props.theme.spacing(6)}px;
+  padding: ${(props) => props.theme.spacing(6)}px;
 
-${props => props.theme.breakpoints.up("md")} {
-  padding: ${props => props.theme.spacing(10)}px;
+  ${(props) => props.theme.breakpoints.up("md")} {
+    padding: ${(props) => props.theme.spacing(10)}px;
   }
 `;
 
 function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: username,
+      password: password,
+      email: email,
+    };
+
+    axios
+      .post("http://localhost:5000/users/register", user)
+      .then((res) => console.log(res.data));
+
+    setUsername("");
+    setPassword("");
+    setEmail("");
+  };
+
   return (
     <Wrapper>
       <Helmet title="Sign Up" />
       <Typography component="h1" variant="h4" align="center" gutterBottom>
-        Get started
+        BCS Dashboard Signup
       </Typography>
       <Typography component="h2" variant="body1" align="center">
-        Start creating the best possible user experience for you customers
+        Create an account to start your BCS journey.
       </Typography>
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormControl margin="normal" required fullWidth>
-          <InputLabel htmlFor="name">Name</InputLabel>
-          <Input id="name" name="name" autoFocus />
-        </FormControl>
-        <FormControl margin="normal" required fullWidth>
-          <InputLabel htmlFor="company">Company</InputLabel>
-          <Input id="company" name="company" />
+          <InputLabel htmlFor="name">Username</InputLabel>
+          <Input
+            id="name"
+            name="name"
+            onChange={(e) => setUsername(e.target.value)}
+            autoFocus
+            required
+          />
         </FormControl>
         <FormControl margin="normal" required fullWidth>
           <InputLabel htmlFor="email">Email Address</InputLabel>
-          <Input id="email" name="email" autoComplete="email" />
+          <Input
+            id="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="email"
+            required
+          />
         </FormControl>
         <FormControl margin="normal" required fullWidth>
-          <InputLabel htmlFor="password">Password</InputLabel>
+          <InputLabel
+            htmlFor="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          >
+            Password
+          </InputLabel>
           <Input
             name="password"
             type="password"
@@ -63,6 +99,7 @@ function SignUp() {
           variant="contained"
           color="primary"
           mt={2}
+          type="submit"
         >
           Sign up
         </Button>
