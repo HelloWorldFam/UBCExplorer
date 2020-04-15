5474abeaf243d274b25c8695798ea9ac8b6ec76f
Jerry <hall.jerry9@gmail.com>
2020-04-14 19:44:43 -0700

    Replaced bcs-dashboard with template

--- bcs-dashboard/src/App.js
+++ bcs-dashboard/src/App.js
@@ -1,32 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Home from "./components/home.component";
-import SignupScreen from "./components/login-screen.component";
-import Dashboard from "./components/dashboard.component";
-
-import Button from '@material-ui/core/Button';
-import Drawer from '@material-ui/core/Drawer';
-
-import TimeTable from "./components/timetable.component.js";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className = "router">
-
-      <Router>
-        
-        <Route path="/" component = {Home} />
-        <Route path="/signup" component = {SignupScreen} />               
-        <Route path="/dashboard" component = {Dashboard}/>   
-        <Route path ="/TimeTable" component = {TimeTable}/> 
-                 
-      </Router>
-     
-     
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>"Hello world"</p>
+      </header>
     </div>
   );
 }
