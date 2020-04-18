755421b3496c3de19c73abcf924a4437441effa3
Scott <scott.m.king@outlook.com>
2020-04-18 15:44:21 -0700

    Got everything working with react-materialize

--- bcs-dashboard/src/pages/landingpage/Banner/Banner.js
+++ bcs-dashboard/src/pages/landingpage/Banner/Banner.js
@@ -1,48 +1,49 @@
-import React, { Component } from "react";
-import "materialize-css/dist/css/materialize.min.css";
+import React from "react";
 import FirstPic from "../Pictures/image1.jpg";
 import SecondPic from "../Pictures/image2.jpg";
 import ThirdPic from "../Pictures/image3.jpg";
 import { Link } from "react-router-dom";
 import "./Banner.css";
+import "materialize-css";
+import { Slider, Slide, Caption } from "react-materialize";
 
-export default class Banner extends Component {
-  render() {
-    return (
-      <section className="slider" id="home">
-        <ul className="slides"> 
-          <li>
-            <img src={FirstPic} alt="Study" />
-            <div className="caption right-align">
-              <h2>BCS Course Selector App</h2>
-              <h5 className="">Welcome to your journey at UBC</h5>
-              <Link to="#" className="btn btn-large blue">
-                Get Started
-              </Link>
-            </div>
-          </li>
-          <li>
-            <img src={SecondPic} alt="Plan your timetable" />
-            <div className="caption left-align">
-              <h2>Course Schedule</h2>
-              <h5 className="">Plan your degree</h5>
-              <Link to="#" className="btn btn-large blue">
-                Explore Now
-              </Link>
-            </div>
-          </li>
-          <li>
-            <img src={ThirdPic} alt="Find out your classmates" />
-            <div className="caption center-align">
-              <h2>Find your classmates</h2>
-              <h5 className="">Connect with peers and alumni</h5>
-              <Link to="#" className="btn btn-large blue">
-                Find Out More
-              </Link>
-            </div>
-          </li>
-        </ul>
-      </section>
-    );
-  }
+// for documentation on react-materialize: http://react-materialize.github.io/react-materialize/
+export default function Banner() {
+  return (
+    <Slider
+      fullscreen={false}
+      options={{
+        height: 650,
+        indicators: true,
+      }}
+    >
+      <Slide image={<img alt="" src={FirstPic} />}>
+        <Caption placement="right">
+          <h2>BCS Course Selector App</h2>
+          <h5 className="">Welcome to your journey at UBC</h5>
+          <Link to="#" className="btn btn-large blue">
+            Get Started
+          </Link>
+        </Caption>
+      </Slide>
+      <Slide image={<img alt="" src={SecondPic} />}>
+        <Caption placement="left">
+          <h2>Course Schedule</h2>
+          <h5 className="">Plan your degree</h5>
+          <Link to="#" className="btn btn-large blue">
+            Explore Now
+          </Link>
+        </Caption>
+      </Slide>
+      <Slide image={<img alt="" src={ThirdPic} />}>
+        <Caption placement="right">
+          <h2>Find your classmates</h2>
+          <h5 className="">Connect with peers and alumni</h5>
+          <Link to="#" className="btn btn-large blue">
+            Find Out More
+          </Link>
+        </Caption>
+      </Slide>
+    </Slider>
+  );
 }
