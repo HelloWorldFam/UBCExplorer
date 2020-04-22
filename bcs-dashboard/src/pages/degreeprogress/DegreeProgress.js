a3fca375ea78bd10e292dcb942c9d9143c1e71ec
Jerry <hall.jerry9@gmail.com>
2020-04-21 17:51:11 -0700

    Added temporary timeline to degreeprogress

--- bcs-dashboard/src/pages/degreeprogress/DegreeProgress.js
+++ bcs-dashboard/src/pages/degreeprogress/DegreeProgress.js
@@ -2,22 +2,26 @@ import React from "react";
 import styled from "styled-components";
 import { NavLink as RouterNavLink } from "react-router-dom";
 
+// Vertical Timeline (Scrolling) component
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import './style.min.css';
+
 import Helmet from 'react-helmet';
 
 import {
-  CardContent,
-  Grid,
-  Link,
-  Breadcrumbs as MuiBreadcrumbs,
-  Card as MuiCard,
-  Divider as MuiDivider,
-  Typography
+    CardContent,
+    Grid,
+    Link,
+    Breadcrumbs as MuiBreadcrumbs,
+    Card as MuiCard,
+    Divider as MuiDivider,
+    Typography
 } from "@material-ui/core";
 
 import { spacing } from "@material-ui/system";
 
 const NavLink = React.forwardRef((props, ref) => (
-  <RouterNavLink innerRef={ref} {...props} />
+    <RouterNavLink innerRef={ref} {...props} />
 ));
 
 const Card = styled(MuiCard)(spacing);
@@ -27,44 +31,65 @@ const Divider = styled(MuiDivider)(spacing);
 const Breadcrumbs = styled(MuiBreadcrumbs)(spacing);
 
 function Timeline() {
-  return (
-    <Card mb={6}>
-      <CardContent>
-        <Typography variant="h6" gutterBottom>
-          Timeline
-        </Typography>
-        <Typography variant="body2" gutterBottom>
-          Empty card
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card style={{ backgroundColor: "#e3e3e3" }} mb={6}>
+            <CardContent>
+                <Typography variant="h6" paragraph >
+                    Timeline
+                </Typography>
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element"
+                        date="2019 Winter Term 1"
+                        // contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}        // Changes card style
+                        // contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}      // Changes arrow style
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} >
+                        <h3 className="vertical-timeline-element-title">
+                            Course Name:
+                        </h3>
+                        <h4 className="vertical-timeline-element-subtitle">
+                            CPSC 110
+                        </h4>
+                        <br />
+                        <h3 className="vertical-timeline-element-title">
+                            Course Name:
+                        </h3>
+                        <h4 className="vertical-timeline-element-subtitle">
+                            CPSC 121
+                        </h4>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+            </CardContent>
+        </Card>
+    );
 }
 
 function DegreeProgress() {
-  return (
-    <React.Fragment>
-      <Helmet title="Blank" />
-      <Typography variant="h3" gutterBottom display="inline">
-        Degree Progress
-      </Typography>
+    return (
+        <React.Fragment>
+            <Helmet title="Degree Progress" />
+            <Typography variant="h3" gutterBottom display="inline">
+                Degree Progress
+            </Typography>
 
-      <Breadcrumbs aria-label="Breadcrumb" mt={2}>
-        <Link component={NavLink} exact to="/dashboard">
-          Dashboard
-        </Link>
-        <Typography>Degree Progress</Typography>
-      </Breadcrumbs>
+            <Breadcrumbs aria-label="Breadcrumb" mt={2}>
+                <Link component={NavLink} exact to="/dashboard">
+                    Dashboard
+                </Link>
+                <Typography>
+                    Degree Progress
+                </Typography>
+            </Breadcrumbs>
 
-      <Divider my={6} />
+            <Divider my={6} />
 
-      <Grid container spacing={6}>
-        <Grid item xs={12}>
-          <Timeline />
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  );
+            <Grid container spacing={6}>
+                <Grid item xs={12}>
+                    <Timeline />
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    );
 }
 
 export default DegreeProgress;
