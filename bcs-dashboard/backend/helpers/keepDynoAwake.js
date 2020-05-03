988793d84116184d0990b2845e46c786f25ab888
Jerry <hall.jerry9@gmail.com>
2020-05-03 15:01:13 -0700

    Changed keepDynoAwake interval back to 25 min
    Co-authored-by: brendeny <brendenbyee@gmail.com>
    Co-authored-by: Scott <king455@gmail.com>
    Co-authored-by: Ben Cheung <chessben88@gmail.com>

--- bcs-dashboard/backend/helpers/keepDynoAwake.js
+++ bcs-dashboard/backend/helpers/keepDynoAwake.js
@@ -5,14 +5,20 @@
  */
 const fetch = require("node-fetch");
 
-const keepDynoAwake = (url, interval = .5, callback) => {
+/**
+ * 
+ * @param {String} url - the url to ping
+ * @param {Number} interval - the interval (in minutes)
+ * @param {Boolean} callback - the callback function 
+ */
+const keepDynoAwake = (url, interval = 25, callback) => {
     const milliseconds = interval * 60000;
     setTimeout(() => {
 
-        try { 
+        try {
             console.log(`setTimeout called.`);
             // HTTP GET request to the dyno's url
-            fetch(url).then(() => console.log(`Fetching ${url}.`)); 
+            fetch(url).then(() => console.log(`Fetching ${url}.`));
         }
         catch (err) { // catch fetch errors
             console.log(`Error fetching ${url}: ${err.message} 
@@ -30,7 +36,7 @@ const keepDynoAwake = (url, interval = .5, callback) => {
                 // do it all again
                 return keepDynoAwake(url, interval, callback);
             }
-            
+
         }
 
     }, milliseconds);
