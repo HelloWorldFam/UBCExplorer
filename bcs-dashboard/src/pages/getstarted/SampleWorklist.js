d415fa22d6aa5b0cb264471fb0aa49c1d6994a1a
Jerry <hall.jerry9@gmail.com>
2020-05-07 01:39:29 -0700

    Another small change to sample worklist

--- bcs-dashboard/src/pages/getstarted/SampleWorklist.js
+++ bcs-dashboard/src/pages/getstarted/SampleWorklist.js
@@ -1,6 +1,9 @@
 export default function buildWorklist(year) {
     return (
         [{
+            "name": year - 1 + "W1",
+            "courses": []
+        }, {
             "name": year - 1 + "W2",
             "courses": []
         }, {
