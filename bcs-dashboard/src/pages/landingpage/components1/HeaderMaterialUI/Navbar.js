264c356cb0ebe806e94fd31231972e9907701dae
JamesPark <45812677+jpjp12@users.noreply.github.com>
2020-05-09 14:44:34 -0700

    Navbar menu working now

--- bcs-dashboard/src/pages/landingpage/components1/HeaderMaterialUI/Navbar.js
+++ bcs-dashboard/src/pages/landingpage/components1/HeaderMaterialUI/Navbar.js
@@ -2,19 +2,54 @@ import React, {useState} from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import MobileRightMenuSlider from "@material-ui/core/Drawer"
 import "./Navbar.css";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import Helmet from "react-helmet";
+import styled from "styled-components";
 
+// Logos
+import GithubLogo from "./SignInLogos/Github.png";
+import GoogleLogo from "./SignInLogos/Google.png";
+import FacebookLogo from "./SignInLogos/Facebook.png";
+
+// added
+import {
+  Button as MuiButton,
+  CardContent,
+  Fade,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Breadcrumbs as MuiBreadcrumbs,
+  Card as MuiCard,
+  Divider as MuiDivider,
+  ListItemText as MuiListItemText,
+  Paper as MuiPaper,
+  Typography,
+} from "@material-ui/core";
+
+import {
+  Drafts as DraftsIcon,
+  MoveToInbox as InboxIcon,
+  Send as SendIcon,
+} from "@material-ui/icons";
+
+import { spacing } from "@material-ui/system";
 
 import {
 AppBar,
 Toolbar,
-ListItem,
+// ListItem,
 IconButton,
-ListItemIcon,
-ListItemText,
+// ListItemIcon,
+// ListItemText,
 Avatar,
-Divider,
-List,
-Typography,
+// Divider,
+// List,
+// Typography,
 Box
 } from "@material-ui/core";
 
@@ -26,6 +61,81 @@ import {
     ContactMail
 } from "@material-ui/icons";
 
+const ExploreLogo = fetch("/favicon.ico");
+
+const Card = styled(MuiCard)`
+  margin: 10px;
+  box-shadow: none;
+`;
+const Button = styled(MuiButton)`
+  
+`
+
+const Divider = styled(MuiDivider)(spacing);
+
+const Breadcrumbs = styled(MuiBreadcrumbs)(spacing);
+
+const Paper = styled(MuiPaper)(spacing);
+
+const ListItemText = styled(MuiListItemText)(spacing);
+
+
+
+// Login
+// Icon menu component for the login expansion
+class IconMenu extends React.Component {
+    state = {
+      anchorEl: null,
+    };
+  
+    handleClick = (event) => {
+      this.setState({ anchorEl: event.currentTarget });
+    };
+  
+    handleClose = () => {
+      this.setState({ anchorEl: null });
+    };
+  
+    render() {
+      const { anchorEl } = this.state;
+      return (
+        <>
+          <Button
+            aria-owns={anchorEl ? "simple-menu" : undefined}
+            aria-haspopup="true"
+            onClick={this.handleClick}
+            variant="contained"
+            color="buttoncolor"
+            disableRipple={true}
+            disableFocusRipple={true}
+          >
+            Sign In/Register
+          </Button>
+  
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={this.handleClose}
+            color="buttoncolor"
+          >
+            <Card>
+              <Typography variant="h6" style={{ padding: '0 10px 10px 10px', }}>Sign in with an OAuth provider:</Typography>
+              <Button style={{ margin: '3px', }} variant="outlined" href="/auth/google">
+                <strong>Google</strong> <img style={{ width: '16px', height: '16px', marginLeft: '10px' }} src={GoogleLogo}></img>
+              </Button>
+              <Button style={{ margin: '3px', }} variant="outlined" href="/auth/facebook">
+                <strong>Facebook</strong> <img style={{ width: '16px', height: '16px', marginLeft: '10px' }} src={FacebookLogo}></img>
+              </Button>
+              <Button style={{ margin: '3px', }} variant="outlined" href="/auth/github">
+                <strong>Github</strong> <img style={{ width: '52px', height: '16px', marginLeft: '10px' }} src={GithubLogo}></img>
+              </Button>
+            </Card>
+          </Menu>
+        </>
+      );
+    }
+  }
 
 
 // CSS styles
@@ -58,6 +168,10 @@ const menuItems = [
     },
     {
         listIcon: <Apps />,
+        listText: "Features"
+    },
+    {
+        listIcon: <Apps />,
         listText: "About"
     },
     {
@@ -66,7 +180,7 @@ const menuItems = [
     },
     {
         listIcon: <AssignmentInd />,
-        listText: "Login"
+        listText: "Sign In/Register"
     },
 ]
 
@@ -111,28 +225,37 @@ const Navbar = () => {
                     <IconButton onClick={toggleSlider("right", true)}>
                     <ArrowBack style={{color: "white"}}/>
                     </IconButton>
-                    <Typography variant="h3" style={{color: "grey"}}>
-                        UBC Explorer
-                    </Typography>
-                    {/* <div className="toolbar__logo" variant="h5" style={{color: "grey"}}><a href="/bcs">UBC Explorer</a></div> */}
-                    <div className="spacer"/>
-                    <div className="toolbar_navigation-items">
-                         <ul>
-                    {/* <li><a href="/bcs">Home</a></li> */}
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Contact</a></li>
-                    <li><a href="/">Login</a></li>
-                         </ul>
-                         </div>
                     <MobileRightMenuSlider
                     anchor="left"
                         open={state.right}
                         onClose={toggleSlider("right", false)}
                         >
                             {sideList("right")}
-
                     </MobileRightMenuSlider>
-                </Toolbar>
+                    <Typography variant="h3" style={{color: "grey"}}>
+                        UBC Explorer
+                    </Typography>
+                    <div className="spacer"/>
+                    <ul className="right hide-on-med-and-down">
+                <li>
+                  <AnchorLink href="/home">Home</AnchorLink>
+                </li>
+                 
+                <li>
+                  <AnchorLink href="/about">Features</AnchorLink>
+                </li>
+                <li>
+                  <AnchorLink href="./contact">About</AnchorLink>
+                </li>
+                <li>
+                  <AnchorLink href="/Footer">Contact</AnchorLink>
+                </li>
+      
+                <li>
+                  <IconMenu />
+                </li>
+              </ul>
+              </Toolbar>
             </AppBar>
         </Box>
         </>
@@ -140,3 +263,17 @@ const Navbar = () => {
 }
 
 export default Navbar;
+
+
+
+                    // {/* <div className="toolbar__logo" variant="h5" style={{color: "grey"}}><a href="/bcs">UBC Explorer</a></div> */}
+                    // {/* <div className="spacer"/>
+                    // <div className="toolbar_navigation-items"> */}
+                    //      <ul>
+                    // {/* <li><a href="/bcs">Home</a></li> */}
+                    // {/* <li><a href="/">About</a></li>
+                    // <li><a href="/">Contact</a></li>
+                    // <li><a href="/">Login</a></li>
+                    //      </ul>
+                    //      </div> */}
+               
