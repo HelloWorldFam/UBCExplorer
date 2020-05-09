ad88c3f649a947ea98c26fcec2e354910f137c4b
JamesPark <45812677+jpjp12@users.noreply.github.com>
2020-05-09 06:02:45 -0700

    need to fix the dropback issue with nav sidebar menu.

--- bcs-dashboard/src/pages/landingpage/components1/Backdrop/Backdrop.js
+++ bcs-dashboard/src/pages/landingpage/components1/Backdrop/Backdrop.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Backdrop.css'
+import './Backdrop.css';
 
 const backdrop = props => (
-    <div className="backdrop" />
+    <div className="backdrop" onClick={props.click} />
 
 );
 
--- bcs-dashboard/src/pages/landingpage/components1/Toolbar/Toolbar.js
+++ bcs-dashboard/src/pages/landingpage/components1/Toolbar/Toolbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Toolbar.css'
-import DrawerToggleButton from './DrawerToggleButton';
+import DrawerToggleButton from '../DrawerToggleButton';
 
 const toolbar = props => (
     <header className="toolbar">
