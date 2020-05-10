ac26ef28db73173c7ca83f56281fe8041be6a897
Ben Cheung <chessben88@gmail.com>
2020-05-10 00:33:48 -0700

    Added more content to api documentation

--- bcs-dashboard/src/pages/rest-api-doc/RestApiDocs.js
+++ bcs-dashboard/src/pages/rest-api-doc/RestApiDocs.js
@@ -31,6 +31,54 @@ function RestApiDocs() {
         <TaskWrapperContent>
           <Typography variant="body">
             <p>Insert text for API here. Coming soon.</p>
+            <br />
+            <p>
+              The API provided below requires no API key access. For specific
+              user data, user authentication is required. The url is{" "}
+              <code>https://ubcexplorer.io/</code>
+            </p>
+            <h4>User Data EndPoints</h4>
+            <div>
+              <b>/userdata</b> <br /> Retrieves the authenticated user's entire
+              profile. A user profile consists of courses, _id, email,
+              firstName, lastName, and picture. User authentication is required.
+              <br />
+              <br />
+              <b>/downloadUserData</b> <br />
+              Download your user data as a JSON file. User authentication is
+              required.
+            </div>
+            <br />
+            <h4>Course Data EndPoints</h4>
+            <div>
+              <b>/getCourses</b>
+              <br /> Retrieves the authenticated user's list of courses. User
+              authentication is required.
+              <br />
+              <br />
+              <b>/getAllCourses</b> <br /> Retrieves a list of all courses
+              offered by UBC that is shown on UBC's calendar.
+              <br />
+              <br />
+              <b>/getCourseInfo/:code</b>
+              <br /> Query a specific course from courses database. <br />
+              Usage example: <br />
+              https://ubcexplorer.io/getCourseInfo/CPSC%20110
+              <br />
+              <br />
+              <b>/searchAny/:code</b>
+              <br /> Returns the top 8 course objects which matches a request
+              passed in and the course code.
+              <br />
+              Usage example: <br />
+              Search CP: https://ubcexplorer.io/searchAny/CP
+              <br />
+              Search CPSC: https://ubcexplorer.io/searchAny/CPSC
+              <br />
+              Search CPSC: https://ubcexplorer.io/searchAny/CPSC%20310
+              <br />
+              <br />
+            </div>
           </Typography>
         </TaskWrapperContent>
       </TaskWrapper>
