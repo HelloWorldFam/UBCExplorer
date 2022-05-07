16c8462343b0956782aea4dc4b9cd348373285a1
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2022-05-07 16:19:32 -0700

    modified more reddit box styling

--- bcs-dashboard/src/pages/courseselector/RedditBox.jsx
+++ bcs-dashboard/src/pages/courseselector/RedditBox.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { color } from "@material-ui/system";
+import styled from "styled-components";
 
-// https://www.reddit.com/dev/api/#GET_search
+const CourseCardStyling = styled.div`
+  :hover {
+    filter: drop-shadow(10px 10px 0.75rem #d3d3d3);
+    cursor: pointer;
+  }
+`;
 
 function RedditBox({ courseCode, courseNum }) {
   const [redditResult, setRedditResult] = useState([]);
 
   useEffect(() => {
     (async () => {
+      // https://www.reddit.com/dev/api/#GET_search
       const {
         data: {
           data: { children: results },
@@ -37,65 +44,96 @@ function RedditBox({ courseCode, courseNum }) {
 
   return (
     <div className="article-container">
-      <h4>/r/UBC Reddit Box</h4>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img src="https://i.imgur.com/lz8TQ1D.png" height="5%" width="5%" />
+        <a
+          href="https://www.reddit.com/r/UBC/"
+          target="_blank"
+          style={{
+            textDecoration: "none",
+            color: "black",
+          }}
+        >
+          {" "}
+          <h4
+            style={{
+              marginLeft: "10px",
+            }}
+          >
+            /r/UBC{" "}
+          </h4>{" "}
+        </a>
+      </div>
 
       {redditResult &&
         redditResult.map((entry) => {
           return (
-            <a
-              href={entry.url}
-              style={{
-                textDecoration: "none",
-                color: "black",
-              }}
-            >
-              <div
+            <CourseCardStyling>
+              <a
+                href={entry.url}
+                target="_blank"
                 style={{
-                  border: "1px solid grey",
-                  backgroundColor: "white",
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "left",
-                  minHeight: "65px",
-                  padding: "5px",
-                  marginBottom: "10px",
-                  borderRadius: "5px",
+                  textDecoration: "none",
+                  color: "black",
                 }}
               >
                 <div
                   style={{
+                    border: "1px solid grey",
+                    backgroundColor: "white",
                     display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    minWidth: "30px",
+                    flexDirection: "row",
+                    justifyContent: "left",
+                    minHeight: "65px",
+                    padding: "5px",
+                    marginBottom: "10px",
+                    borderRadius: "5px",
                   }}
                 >
-                  <img
-                    src="https://i.imgur.com/PujrP7q.png"
-                    height="10px"
-                    width="10px"
-                  />
-                  <div>{entry.upvotes}</div>
-                </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      minWidth: "30px",
+                    }}
+                  >
+                    <img
+                      src="https://i.imgur.com/PujrP7q.png"
+                      height="10px"
+                      width="10px"
+                    />
+                    <div>{entry.upvotes}</div>
+                  </div>
 
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    marginLeft: "20px",
-                  }}
-                >
-                  {" "}
-                  <p style={{ margin: 0 }}>{entry.title}</p>
-                  <p style={{ margin: 0, marginTop: '5px', fontSize: "12px" }}>
-                    Created At: {entry.date_created.toDateString()}
-                  </p>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      marginLeft: "20px",
+                    }}
+                  >
+                    {" "}
+                    <p style={{ margin: 0 }}>{entry.title}</p>
+                    <p
+                      style={{ margin: 0, marginTop: "5px", fontSize: "12px" }}
+                    >
+                      Created At: {entry.date_created.toDateString()}
+                    </p>
+                  </div>
+                  <br />
                 </div>
-                <br />
-              </div>
-            </a>
+              </a>
+            </CourseCardStyling>
           );
         })}
     </div>
